d3eabf9ca4644263cfcf8f62e1e41a2d8ab0bd3e
Ptrchn_deaw <97272373+Ptrchn@users.noreply.github.com>
2023-10-27 17:08:39 +0700

    new

--- ctp-p-web-data/src/Home.jsx
+++ ctp-p-web-data/src/Home.jsx
@@ -22,15 +22,24 @@ function Home() {
 
     return (
         <>
-            <div className="picture">
-                
-            </div>
-            <ul className="carparkingbox">
-                <li className="carparkingButt">
-                    <Link to="/carparking">Car Parking</Link>
-                </li>
-            </ul>
-            
+            <header>
+            </header>
+            <body>
+                <ul className="carparkingbox">
+                    <li className="carparkingButt">
+                        <Link to="/carparking">Car Parking</Link>
+                    </li>
+                </ul>
+                <div class = "container">
+                        <div><img src="https://img.freepik.com/premium-photo/top-down-aerial-view-many-cars-parking-lot-supermarket-sale-car-dealer-market_127089-10749.jpg"/></div>
+                        <div class = "text">
+                                <p>Hotel Parking Booking</p>
+                                <p>Hotel parking booking websites allow you to pre-book parking at your hotel. 
+                                Go through the search, reserve, and confirmation steps to make parking reservations 
+                                convenient and time-saving.</p>
+                        </div>
+                    </div>
+            </body>
         </>
     );
 }
